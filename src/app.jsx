843e591d93a4f160357ddecb3707433fadcb5ae0
NamgyungKim <namgyung.kim@gmail.com>
2021-11-19 17:21:35 +0900

    back to the list when serching

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,25 @@
 import { useEffect, useState } from 'react';
 import styles from './app.module.css';
 import SerchHeader from './components/search_header/serch_header';
+import VideoDetail from './components/video_detail/video_detail';
 import VideoIist from './components/video_list/video_list';
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([])
+  const [selectedVideo, setSelectedVideo] = useState(null)
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video)
+  }
   const search = query => {
     youtube
       .search(query)
-      .then(videos => setVideos(videos))
+      .then(videos => {
+        setVideos(videos)
+        setSelectedVideo(null)
+      })
   } 
+
   useEffect(()=>{  
     youtube
       .mostPopular()
@@ -19,7 +29,16 @@ function App({ youtube }) {
   return (
   <div className={styles.app}>
     <SerchHeader onSearch={search}/>
-    <VideoIist videos={videos}/>
+    <section className={styles.content}>
+    {selectedVideo && (
+      <div className={styles.detail}>
+        {selectedVideo && <VideoDetail video={selectedVideo} />}
+      </div>
+    )}
+      <div className={styles.list}>
+        <VideoIist videos={videos} onVideoClick={selectVideo} display={ selectedVideo ? 'list' : 'grid' }/>
+      </div>
+    </section>
   </div>
   )
 }
